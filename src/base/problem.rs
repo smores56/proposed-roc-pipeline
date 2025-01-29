3588189fe1e5344a320c3df1300e3587d0ec0c23
Sam Mohr <sam@sammohr.dev>
2025-01-29 04:52:18 -0800

    Add more problem variants

--- src/base/problem.rs
+++ src/base/problem.rs
@@ -9,16 +9,24 @@ pub enum CompilerProblem {
     LiftFunctions(LiftFunctionsProblem),
     SolveFunctions(SolveFunctionsProblem),
     SpecializeFunctions(SpecializeFunctionsProblem),
+    LowerIr(LowerIrProblem),
+    ReferenceCount(ReferenceCountProblem),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SpecializeTypesProblem {}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LiftFunctionsProblem {}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SolveFunctionsProblem {}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SpecializeFunctionsProblem {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum LowerIrProblem {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ReferenceCountProblem {}
